e0f806babaa1ed6978070c795a9d3796829daff4
cocagolau <cocagolau@gmail.com>
2014-10-21 17:17:33 +0900

    padding-top, bottom 버그수

--- webapp/WEB-INF/pages/index.jsp
+++ webapp/WEB-INF/pages/index.jsp
@@ -8,7 +8,6 @@
 <meta name="viewport"
 	content="width=device-width, initial-scale=1.0, , minimum-scale=1.0, maximum-scale=1.0">
 <link rel="stylesheet" type="text/css" href="stylesheets/index.css">
-<script type="text/javascript" src="scripts/app.js"></script>
 </head>
 
 <body>
--- webapp/scripts/app.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', init);
-
-function init(){
-	var sections = document.querySelectorAll('.section');
-	var lastSection = sections[sections.length-1];
-	lastSection.style.marginBottom = "50px";
-	var firstSection = sections[0];
-	firstSection.style.marginTop = "50px";
-}
