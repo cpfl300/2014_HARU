9a4bf4bda6619ef03e8ce3955fd053b35c3dec80
cocagolau <cocagolau@gmail.com>
2014-10-27 21:48:27 +0900

    article 영역 html 태그 작성 /article 로 접근시 aritlce 페이지 보이도록 설정

--- src/main/java/haru/controller/AritlceController.java
+++ src/main/java/haru/controller/AritlceController.java
@@ -1,8 +1,14 @@
 package haru.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AritlceController {
-
+	
+	@RequestMapping("/article")
+	public String articleView() {
+		
+		return "article";
+	}
 }
