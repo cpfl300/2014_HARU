20eb0bb9e96a3c98fb6b7b5aaee9f5d7d7434b5d
cocagolau <cocagolau@gmail.com>
2014-11-01 15:50:00 +0900

    issue #113 #114 #115
    소나 심각한 문제들 해결함

--- src/main/java/haru/filter/SiteMeshFilter.java
+++ src/main/java/haru/filter/SiteMeshFilter.java
@@ -13,23 +13,6 @@ public class SiteMeshFilter extends ConfigurableSiteMeshFilter {
 		
 		builder
 		.addTagRuleBundle (new DivExtractingTagRuleBundle());
-		
-		// Map default decorator. This shall be applied to all paths if no other paths match.
-		//.addDecoratorPath("/*", "/WEB-INF/pages/layouts/layout1.ftl");
-		
-		
-		/*
-		// Map decorators to path patterns.
-		.addDecoratorPath("/admin/*", "/another-decorator.html")
-		.addDecoratorPath("/*.special.jsp", "/special-decorator.html")
-		
-		// Map multiple decorators to the a single path.
-		.addDecoratorPaths("/articles/*", "/decorators/article.html", "/decoratos/two-page-layout.html", "/decorators/common.html")
-		
-		// Exclude path from decoration.
-		.addExcludedPath("/javadoc/*")
-		.addExcludedPath("/brochures/*");
-		 */
 	}
 
 }
