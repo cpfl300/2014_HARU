cb61a5d650916448772a5b909705ecf7b634f0e2
cpfl300 <cpfl300@naver.com>
2014-11-06 04:28:18 +0900

    issue #104 화면전환 완료.

--- webapp/WEB-INF/scripts/main/app/article.js
+++ webapp/WEB-INF/scripts/main/app/article.js
@@ -1,15 +1,19 @@
-//flicking 인식모듈 test
 
 window.addEventListener('DOMContentLoaded', init);
 
 function init(){
 	var container = document.querySelector('#container');
-	Flick.flicking(container, 80, changeLeftPos.bind(this, container.firstElementChild, -window.innerWidth), changeLeftPos.bind(this, container.firstElementChild, window.innerWidth));
-
+	Flick.flicking(container, 80, toggleClass.bind(this, "article-container", "flipped"), null);
+	
 }
 
+//flicking 인식모듈 test 
 function changeLeftPos(ele, movePos){
 	var curPos = parseInt(ele.style.left);
 	if(isNaN(curPos)) curPos = 0;
 	ele.style.left = (curPos + movePos) + "px";
+}
+
+function toggleClass(className, toggleClass){
+	$('.'+className).toggleClass(toggleClass);
 }
