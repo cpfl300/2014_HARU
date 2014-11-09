a5eb185933f170faff6ed451b7aa57c77b1d5e6c
cpfl300 <cpfl300@naver.com>
2014-11-10 08:28:16 +0900

    responseBody를 사용하는 json변환 질문을 위한 branch

--- src/main/java/haru/api/controller/AritlceController.java
+++ src/main/java/haru/api/controller/AritlceController.java
@@ -6,7 +6,9 @@ import haru.model.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api/v1")
@@ -15,12 +17,14 @@ public class AritlceController {
 	@Autowired
 	private ArticleDao articleDao;
 	
-	@RequestMapping("/article")
-	public String getArticle(Model model) {
+	@RequestMapping("/article/{issue_num}/{article_num}")
+	public @ResponseBody Article getArticle(@PathVariable("issue_num") String issue_num, @PathVariable("article_num") String article_num) {
+		System.out.println("INN");
+//		Article article = articleDao.get("article1");
+//		model.addAttribute("article", article);
 		
-		Article article = articleDao.get("article1");
-		model.addAttribute("article", article);
+		Article article = new Article();
 		
-		return "article";
+		return article;
 	}
 }
--- src/main/java/haru/config/HaruServletConfig.java
+++ src/main/java/haru/config/HaruServletConfig.java
@@ -1,16 +1,24 @@
 package haru.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 @org.springframework.context.annotation.Configuration
-@ComponentScan(basePackages={"haru.controller"})
+@ComponentScan(basePackages={"haru.controller", "haru.api.controller"})
 @EnableWebMvc
 public class HaruServletConfig extends WebMvcConfigurerAdapter{
 	
@@ -37,6 +45,32 @@ public class HaruServletConfig extends WebMvcConfigurerAdapter{
 	    return configurer;
 
 	}
+
+//	@Bean
+//	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+//		MappingJackson2HttpMessageConverter m = new MappingJackson2HttpMessageConverter();
+//		List<MediaType> types = new ArrayList<MediaType>();
+//		MediaType type = new MediaType("application/json");
+//		types.add(type);
+//		m.setSupportedMediaTypes(types);
+//		return m;
+//	}
+//	
+//	@Bean
+//	public AnnotationMethodHandlerAdapter annotationMethodHandlerAdapter() {
+//		
+//		AnnotationMethodHandlerAdapter adapter = new AnnotationMethodHandlerAdapter();
+//		
+//		List<HttpMessageConverter<?>> converters = new ArrayList<HttpMessageConverter<?>>();
+//		HttpMessageConverter<?> ary[] = new HttpMessageConverter[converters.size()];
+//		
+//		adapter.setMessageConverters(converters.toArray(ary));
+//		
+//		return adapter;
+//		
+//	}
+
+	
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
