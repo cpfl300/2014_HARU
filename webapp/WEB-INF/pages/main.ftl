184c354fe1aa4f5e5bdc3dfab1b948dc8a5020ef
cpfl300 <cpfl300@naver.com>
2014-11-25 12:46:35 +0900

    issue #163 라이브러리 교체 pep.js -> iscroll.js (pep.js는 너무 무거움)

--- webapp/WEB-INF/pages/main.ftl
+++ webapp/WEB-INF/pages/main.ftl
@@ -1,33 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>main view</title>
-	<link rel="stylesheet" type="text/css" href="/stylesheets/main.css" />
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-	<script type="text/javascript" src="http://rawgit.com/briangonzalez/jquery.pep.js/master/src/jquery.pep.js"></script>
-	<script type="text/javascript" src="/scripts/main/app/APP.list-move.js"></script>
+<title>main view</title>
+<link rel="stylesheet" type="text/css" href="/stylesheets/main.css" />
+<script type="text/javascript"
+	src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script type="text/javascript" src="/scripts/main/cage/CAGE.iscroll.js"></script>
+<script type="text/javascript" src="/scripts/main/app/APP.list-move.js"></script>
 </head>
-	<body>
-		<div class="list-background">
-			<div class="list-move-container">
-				<div class="list-date-container">
-					<div class="datetime"> 
-						<img src="http://cdn.wallwuzz.com/uploads/tokyo-wallpaper-photography-city-scape-cityscape-grayscale-styles-cityscapes-wallwuzz-hd-wallpaper-20342.jpeg"/>
-						<p class="whiten"> 2014.11.11 </p> 
-						<p> 2014.11.11 </p> 
-					</div>
-				</div>
-				<div class="list-lists">
-					<#list haruHotissues as article>
-						<div class="issue-container">
-						 	<div class="issue">
-						 		${article.hotissue}
-						 	</div>
-						 </div>
-					</#list>
+<body>
+	<div class="list-background">
+		<div class="list-move-container">
+			<div class="list-date-container">
+				<div class="datetime">
+					<!-- <img src="http://cdn.wallwuzz.com/uploads/tokyo-wallpaper-photography-city-scape-cityscape-grayscale-styles-cityscapes-wallwuzz-hd-wallpaper-20342.jpeg"/> -->
+					<p class="whiten"></p>
+					<p>2014.11.11</p>
 				</div>
 			</div>
-			<img src="http://cdn.wallwuzz.com/uploads/tokyo-wallpaper-photography-city-scape-cityscape-grayscale-styles-cityscapes-wallwuzz-hd-wallpaper-20342.jpeg"/>
+			<div class="list-lists">
+				<#list haruHotissues as article>
+				<div class="issue-container">
+					<div class="issue">${article.hotissue}</div>
+				</div>
+				</#list>
+			</div>
 		</div>
-	</body>
+	</div>
+	<img
+		src="http://cdn.wallwuzz.com/uploads/tokyo-wallpaper-photography-city-scape-cityscape-grayscale-styles-cityscapes-wallwuzz-hd-wallpaper-20342.jpeg" />
+</body>
 </html>
--- webapp/WEB-INF/scripts/main/app/APP.list-move.js
+++ webapp/WEB-INF/scripts/main/app/APP.list-move.js
@@ -1,44 +1,15 @@
 window.addEventListener('load', list_move_init);
-
+document.addEventListener('DOMContentLoaded', setPos);
 function list_move_init(){
-	setPos();
 	MoveList.init($('.list-move-container'));
+	
 }
 
 var MoveList = {
 		init: function(target){
-			target.pep({
-//				moveTo : function(x,y){
-////					this.$el -> 움직일 엘리먼트
-//				},
-				useCSSTranslation: false,
-				axis: 'y',
-				velocityMultiplier: 2.5,
-				start: function(){
-					this.currentTop = parseFloat($('.list-move-container')[0].style.top);
-//					console.log(this); // MoveList
-				}.bind(this),
-				easing: function(){
-					var moveDistance = parseFloat($('.list-move-container')[0].style.top) - this.currentTop;
-					console.log(moveDistance);
-					this.currentTop = parseFloat($('.list-move-container')[0].style.top);
-				}.bind(this)
-				
-			});
-			
 			this.target = target[0];
+			var myScroll = new IScroll('.list-background', { mouseWheel: true });
 			this.eventBind();
-			
-//			clock
-//			this.clock = document.querySelector('.datetime');
-//			this.clockOpacity = 1;
-//			this.clock.style.opacity = 1;
-			
-//			header background
-//			this.header = document.querySelector('#header');
-//			this.header.style.backgroundColor = "rgba(255, 255, 255, 0)";
-//			this.headerOpacity = parseInt(this.header.style.backgroundColor.split(", ")[3].split(")")[0]);
-			
 		},
 		eventBind: function(){
 			this.target.addEventListener('touchstart', this.touchstart.bind(this));
@@ -50,35 +21,9 @@ var MoveList = {
 		},
 		touchmove: function(e){
 			e.preventDefault();
-//			var moveDistance = this.curPoint - e.changedTouches[0].pageY;
-			
-//			this.clockOpacity -= moveDistance/100;
-			
-//			if(-320 <= parseInt(this.curTop) && parseInt(this.curTop) <= -240){
-//				this.headerOpacity += moveDistance*0.01089;
-//			}
-			
 		},
 		touchend: function(e){
 			e.preventDefault();
-			setTimeout(function(){this.boundaryCheck()}.bind(this), 350);
-//			var minimumMargin = -(document.querySelector('.list-lists').children.length * 65) + (65*3);
-//			if(this.curTop > 0){
-//				this.curTop = 0;
-//				this.clockOpacity = 1;
-//				
-//				this.headerOpacity = 0;
-//			}
-//			if(this.curTop < minimumMargin){
-//				this.curTop = minimumMargin;
-//				this.clockOpacity = 1 + (minimumMargin/100);
-//				
-//				this.headerOpacity = 0.894118;
-//			}
-//			
-//			this.target.style.webkitTransform = 'translate3d(0, '+ this.curTop +'px, 0)';
-//			this.clock.style.opacity = this.clockOpacity;
-			
 		},
 		animate: function(){
 			this.clock.style.opacity = this.clockOpacity;
@@ -94,10 +39,12 @@ var MoveList = {
 			if(curTop > 0){
 				$('.list-move-container')[0].style.top = '0px';
 			}
-		}
-}
+		},
+};
 
 function setPos(){
-	var datetimePos = $('.datetime')[0].offsetTop;
-	$('.datetime img')[0].style.marginTop= -datetimePos+'px';
+	$('.list-date-container')[0].style.height = $(window).height()*0.6 + 'px';
+	
+//	var datetimePos = $('.datetime').offset().top;
+//	$('.datetime img')[0].style.marginTop= -datetimePos+'px';
 }
